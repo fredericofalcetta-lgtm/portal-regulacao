2e9dbb25b0f49ecf5af180cfd2eb2cbf70e00d96
fredericofalcetta-lgtm <frederico.falcetta@telessauders.org.br>
2026-04-30 09:19:27 -0300

    Add files via upload

---
+++ client/src/const.ts
@@ -0,0 +1,4 @@
+export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
+
+// URL de login aponta para o endpoint do servidor que redireciona ao Google
+export const getLoginUrl = () => "/api/auth/login";
