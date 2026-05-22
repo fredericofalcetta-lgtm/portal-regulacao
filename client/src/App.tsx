f8b81a2b35a04aca433daa423a9b508d570b6ca6
fredericofalcetta-lgtm <frederico.falcetta@telessauders.org.br>
2026-05-22 16:45:01 -0300

    Add route for monitoring logins

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -99,6 +99,13 @@ function Router() {
           </AuthGuard>
         )}
       </Route>
+      <Route path="/monitor-logins">
+        {() => (
+          <AuthGuard>
+            <Home />
+          </AuthGuard>
+        )}
+      </Route>
 
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
